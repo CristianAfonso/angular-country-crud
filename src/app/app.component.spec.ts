f0976829df6b3c45d4f1b3df65506022c18129b3
can97 <can97moya@gmail.com>
2022-04-08 18:05:09 +0100

    Terminado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-country-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-country-crud');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import {Country} from './models/country';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-country-crud';
+
+  selectedCountry: Country = new Country();
+
+  countryArray: Country[] = [
+    {id: 1, name: "Spain", capital:"Madrid", languaje:"Spanish"},
+    {id: 2, name: "England", capital:"London", languaje:"English"},
+    {id: 3, name: "France", capital:"Paris", languaje:"French"}
+  ];
+
+  addOrEdit(){
+    if(this.selectedCountry.id === 0){
+      this.selectedCountry.id = this.countryArray.length + 1;
+      this.countryArray.push(this.selectedCountry);
+    }
+    this.selectedCountry = new Country();
+  }
+
+  openToEdit(country: Country){
+    this.selectedCountry = country;
+  }
+
+  delete(){
+    if(confirm('This will delete the country, Are you sure?')){
+      this.countryArray = this.countryArray.filter(x => x != this.selectedCountry);
+      this.selectedCountry = new Country();
+    }
+
+  }
+
+
 }
